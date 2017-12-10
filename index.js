e49c32f79d0274bc40ec0b2e6abce55a77601c7f
MICHAEL-PC\micha <michaelroether@gmx.net>
2017-12-10 18:18:06 +0100

    improving (tested)

--- index.js
+++ index.js
@@ -18,34 +18,35 @@ app.get('/', function(request, response) {
   response.send("Funktioniert!");
 });
 
-
 // POST Request to Dialogflow Webhook
 app.post('/info', function(request, response) {
     var forename = request.body.result && request.body.result.parameters && request.body.result.parameters.playerforename ? request.body.result.parameters.playerforename : "Kein Vorname.";
     var lastname = request.body.result && request.body.result.parameters && request.body.result.parameters.playerlastname ? request.body.result.parameters.playerlastname : "Kein Nachname.";
     var information = request.body.result && request.body.result.parameters && request.body.result.parameters.information ? request.body.result.parameters.information : "Keine Information.";
     var teamname = request.body.result && request.body.result.parameters && request.body.result.parameters.teamname ? request.body.result.parameters.teamname : "Kein Teamname.";
-    // Start the request
-    const as = require('request');
 
-    // POST Request to Chromecast Backend
-    // Set the headers
-    const headers = {
-        'Content-Type': 'application/json'
-    };
-    // Configure the request
-    const options = {
-        url: 'https://zdf-voice-app.herokuapp.com/playerInfo',
-        method: 'POST',
-        headers: headers,
-        form: {'forename': forename, 'lastname': lastname}
-    };
+        // POST Request to Chromecast Backend
+        // Start the request
+        const requestservice = require('request');
+        // Set the headers
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+        // Configure the request
+        const options = {
+            url: 'https://zdf-voice-app.herokuapp.com/playerInfo',
+            method: 'POST',
+            headers: headers,
+            form: {'forename': forename, 'lastname': lastname}
+        };
+        // Send the request
+        requestservice(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                console.log(body);}
+        });
 
-    as(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // Print out the response body
-            console.log(body);}
-    });
+    // Return parameters to Dialogflow
     return response.json({
         speech: forename + " " + lastname + ". Information: " + information + ". Team: " + teamname + ".",
         displayText: forename + " " + lastname + ". Information: " + information + ". Team: " + teamname + ".",
@@ -53,7 +54,6 @@ app.post('/info', function(request, response) {
     });
 
 });
-
 
 // Initialize Server
 app.listen((process.env.PORT || 8080), function() {
